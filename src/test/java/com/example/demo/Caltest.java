751ac2e7f8d7c25f4319ed7c1337cff6dfd88efb
thanush <thanush.pk@sonata-software.com>
2022-08-24 17:42:28 +0530

    first commit

--- src/test/java/com/example/demo/Caltest.java
+++ src/test/java/com/example/demo/Caltest.java
@@ -16,7 +16,7 @@ class Caltest {
 	@Test
 	public void divtest() {
 		//int x= c.div(1,1);
-		assertThrows(ArithmeticException.class,()->c.div(1,0),"this is an error");
+		assertThrows(ArithmeticException.class,()->c.div(10,2),"this is an error");
 	}
 
 }
